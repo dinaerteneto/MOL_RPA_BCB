15bbc2f4d54eedd3bf4bc300c2dae3a754c024a6
Dinaerte Neto <dinaerte.neto@farme.com.br>
2024-12-12 15:22:23 -0300

    Separados em Steps os passos para chegar até o formulário e adicionado submissão do formulário.

--- MOL_RPA_BCB/Services/SeleniumCotacaoService.cs
+++ MOL_RPA_BCB/Services/SeleniumCotacaoService.cs
@@ -25,24 +25,11 @@ public class SeleniumCotacaoService : ICotacaoService
 
             // implementar navegaçõe e extração dos dados usando Selenium
             // Aguarde o carregamento inicial
-            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
 
-            // Navegar pelos menus usando o método reutilizável
-            ClickElement(wait, "#navbarDropdown2"); // Estabilidade financeira
-            ClickElement(wait, "#navbarSupportedContent > ul > li.nav-item.dropdown.show > div > div > div > div.col-3.nivel.segundo-nivel > div > div > a:nth-child(5)"); // Câmbio e Capitais internacionais
-            ClickElement(wait, "#navbarSupportedContent > ul > li.nav-item.dropdown.show > div > div > div > div:nth-child(6) > div > div > a:nth-child(2)"); // Cotação de moedas
-            ClickElement(wait, "#navbarSupportedContent > ul > li.nav-item.dropdown.show > div > div > div > div:nth-child(38) > div > div > a:nth-child(4)"); // Consulta de cotações e boletins
-
-            Console.WriteLine("Todos os menus foram clicados com sucesso!");
-
-            // Certifique-se de que chegou na página correta
-            Console.WriteLine("Título da página atual: " + _driver.Title);
-
-            Console.WriteLine("Todos os menus foram clicados com sucesso!");
-
-            // Certifique-se de que chegou na página correta
-            Console.WriteLine("Título da página atual: " + _driver.Title);
+            NavegarParaFormulario(wait);
+            SubmeterFormulario(wait, inicio, fim, moedaBase);
 
         }
         catch (Exception ex)
@@ -51,13 +38,85 @@ public class SeleniumCotacaoService : ICotacaoService
         }
         finally {
             if (_driver != null) { 
-                // _driver.Quit();
-                // _driver.Dispose();
+                _driver.Quit();
+                _driver.Dispose();
             }
         }
         
         return await Task.FromResult( cotacoes );
     }
+
+
+    /// <summary>
+    /// Navega pelo menu para abrir a página que contém o formulário
+    /// </summary>
+    /// <param name = "wait" > Instância do WebDriverWait.</param>
+    private void NavegarParaFormulario(WebDriverWait wait)
+    {
+        var menuSteps = new List<(string Selector, string Description)>
+        {
+            ("#navbarDropdown2", "Estabilidade financeira"),
+            ("#navbarSupportedContent > ul > li.nav-item.dropdown.show > div > div > div > div.col-3.nivel.segundo-nivel > div > div > a:nth-child(5)", "Câmbio e Capitais internacionais"),
+            ("#navbarSupportedContent > ul > li.nav-item.dropdown.show > div > div > div > div:nth-child(6) > div > div > a:nth-child(2)", "Cotação de moedas"),
+            ("#navbarSupportedContent > ul > li.nav-item.dropdown.show > div > div > div > div:nth-child(38) > div > div > a:nth-child(4)", "Consulta de cotações e boletins")
+        };
+
+        foreach (var (selector, description) in menuSteps)
+        {
+            Console.WriteLine($"Clicando em: {description}");
+            ClickElement(wait, selector);
+        }
+
+        Console.WriteLine("Todos os menus navegados com sucesso!");
+    }
+
+
+    /// <summary>
+    /// Método auxiliar que combina a manipulação dos elementos do formulário 
+    /// para submete-lo
+    /// </summary>
+    /// <param name="wait">Instância do WebDriverWait.</param>
+    /// <param name="inicio">Data inicial que será digitada no formulário</param>
+    /// <param name="fim">Data final que será digitada no formulário</param>
+    /// <param name="moedaBase">Nome da moeda a ser pesquisada</param>
+    private void SubmeterFormulario(WebDriverWait wait, DateTime inicio, DateTime fim, string moedaBase)
+    {
+        // Trocar para o iframe
+        IWebElement iframe = wait.Until(d => d.FindElement(By.CssSelector("body > app-root > app-root > div > div > main > dynamic-comp > div > div:nth-child(3) > div.col-md-8 > div > iframe")));
+        _driver.SwitchTo().Frame(iframe);
+
+        Console.WriteLine("Contexto alterado para o iframe.");
+
+        // Selecionar o radio button
+        IWebElement radioButton = wait.Until(d => d.FindElement(By.CssSelector("input[type='radio'][name='RadOpcao'][value='1']")));
+        radioButton.Click();
+        Console.WriteLine("Primeira opção do radio button selecionada!");
+
+        // Preencher datas
+        var dataInicial = inicio.ToString("ddMMyyyy");
+        var dataFinal = fim.ToString("ddMMyyyy");
+        FillInputField(wait, "#DATAINI", dataInicial);
+        Console.WriteLine($"Data inicial digitida: {dataInicial}");
+        FillInputField(wait, "#DATAFIM", dataFinal);
+        Console.WriteLine($"Data final digitida: {dataFinal}");
+        Console.WriteLine("Datas preenchidas!");
+
+        // Selecionar moeda
+        IWebElement moedaInput = wait.Until(d => d.FindElement(By.CssSelector("body > div > form > table:nth-child(3) > tbody > tr:nth-child(4) > td:nth-child(2) > select")));
+        moedaInput.Click();
+        moedaInput.SendKeys(moedaBase);
+        moedaInput.SendKeys(Keys.Enter);
+        Console.WriteLine("Moeda selecionada!");
+
+        // Submeter formulário
+        ClickElement(wait, "body > div > form > div > input");
+        Console.WriteLine("Formulário submetido!");
+
+        // Voltar ao contexto principal
+        _driver.SwitchTo().DefaultContent();
+        Console.WriteLine("Contexto alterado de volta para o conteúdo principal.");
+    }
+
 
 
     /// <summary>
@@ -80,6 +139,33 @@ public class SeleniumCotacaoService : ICotacaoService
         catch (Exception ex)
         {
             Console.WriteLine($"Erro ao clicar no elemento com seletor '{cssSelector}': {ex.Message}");
+            throw;
+        }
+    }
+
+
+    /// <summary>
+    /// Método para preencher campos de input
+    /// </summary>
+    /// <param name="wait">Instância do WebDriverWait.</param>
+    /// <param name="cssSelector">Seletor CSS do elemento.</param>
+    /// <param name="value">Valor a ser inserido no input</param>
+    private void FillInputField(WebDriverWait wait, string cssSelector, string value)
+    {
+        try
+        {
+            IWebElement inputField = wait.Until(d => d.FindElement(By.CssSelector(cssSelector)));
+            inputField.Clear();
+            inputField.SendKeys(value);
+        }
+        catch (WebDriverTimeoutException)
+        {
+            Console.WriteLine($"Campo de entrada com seletor '{cssSelector}' não foi encontrado dentro do tempo limite.");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao preencher o campo de entrada com seletor '{cssSelector}': {ex.Message}");
             throw;
         }
     }
